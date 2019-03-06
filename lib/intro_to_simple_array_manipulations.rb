fbc24ff55c34d65e6357de8307bee5f802a5f581
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-06 04:26:27 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,12 +9,12 @@ end
  def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+  neighborhoods_in_northwest_brooklyn.unshift(new_neighborhood)
 end
 
  def using_pop(great_hits_of_the_nineties)
-  @great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
-  @great_hits_of_the_nineties.pop
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  great_hits_of_the_nineties.pop
 end
 
  def pop_with_args(chars_in_game_of_thrones)
