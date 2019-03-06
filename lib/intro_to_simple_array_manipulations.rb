011e9b0d1c733d5e834cf76b73760dd8059b1e05
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-06 04:22:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,6 @@ def using_push(countries_in_western_africa, next_country)
   countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
   countries_in_western_africa.push(next_country)
-  binding.pry
 end
 
  def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
